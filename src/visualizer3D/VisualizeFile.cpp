875082f8aee0015bea93531ee9913757cc452cf9
Nut <enrico.mannocci99@gmail.com>
2020-10-22 21:18:05 +0200

    Apre Nuova Macchina

--- src/visualizer3D/VisualizeFile.cpp
+++ src/visualizer3D/VisualizeFile.cpp
@@ -26,18 +26,18 @@ int width, height;
 float aspect;
 glm::mat4 pMat, vMat, mMat, mvMat;
 
-ImportedModel cube("./BlenderImport/Moto.obj");
+ImportedModel cube("./BlenderImport/Carrozzeria.obj");
 
 void setupVertices(void){
     std::vector<glm::vec3> vert = cube.getVertices();
     std::vector<glm::vec2> tex = cube.getTextCoords();
     std::vector<glm::vec3> norm = cube.getNormalVecs();
     int numObjVertices = cube.getNumVertices();
-    
+
     std::vector<float> pvalues; //vertex positions
     std::vector<float> tvalues; //texture coordinates
     std::vector<float> nvalues; //normal vectors
-    
+
     for (int i=0; i<numObjVertices; i++){
         pvalues.push_back((vert[i]).x);
         pvalues.push_back((vert[i]).y);
@@ -51,15 +51,15 @@ void setupVertices(void){
     glGenVertexArrays(1, vao);
     glBindVertexArray(vao[0]);
     glGenBuffers(numVBOs, vbo);
-    
+
     //VBO for vertex location
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
     glBufferData(GL_ARRAY_BUFFER, pvalues.size()*4, &pvalues[0], GL_STATIC_DRAW);
-    
+
     //VBO for texture coordinates
     glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
     glBufferData(GL_ARRAY_BUFFER, tvalues.size()*4, &tvalues[0], GL_STATIC_DRAW);
-    
+
     //VBO for normal vectors
     glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
     glBufferData(GL_ARRAY_BUFFER, nvalues.size()*4, &nvalues[0], GL_STATIC_DRAW);
@@ -67,8 +67,8 @@ void setupVertices(void){
 
 void init (GLFWwindow* window){
     renderingProgram = createShaderProgram((char *)"./BlenderImport/vertShader.glsl",(char *) "./BlenderImport/fragShader.glsl");
-    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 12.0f;
-    cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 300.0f;
+    cubeLocX = 0.0f; cubeLocY = -4.0f; cubeLocZ = -4.0f;
     setupVertices();
 }
 
@@ -77,26 +77,26 @@ void display (GLFWwindow* window, double currentTime){
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT); //Clear the background to black
     glUseProgram(renderingProgram);
-    
+
     //Getto le uniform
     mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
     projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
-    
+
     //Costruisco la pMat
     glfwGetFramebufferSize(window, &width, &height);
     aspect = (float)width / (float)height;
     pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); //1.0472 radians = 60 degrees
-    
+
     //Costruisco la mvMat
     vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
     mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
     mMat = glm::rotate(mMat, 1.75f*(float)currentTime, glm::vec3(0.0f, 1.0f, 0.0f));
     mvMat = vMat * mMat;
-    
+
     //Spedisco matrici allo shader
     glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
-    
+
     //Associazione VBO
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
