a24eaf3179e67303872885ced54384944664ec36
Nut <enrico.mannocci99@gmail.com>
2020-10-26 13:08:02 +0100

    Visuale funzionante

--- src/visualizer3D/VisualizeTrack/TrackVisualizer.cpp
+++ src/visualizer3D/VisualizeTrack/TrackVisualizer.cpp
@@ -45,6 +45,9 @@ glm::mat4 pMat, vMat, mMat, mvMat;
  * mvMat : vMat*mMat
 */
 
+//Funzioni
+glm::vec3 rotate(float, float, float, glm::vec3, float);
+
 void setupVertices(){
     /*float arr[18] = {
                 -0.5, -0.5, 0,
@@ -65,7 +68,7 @@ void setupVertices(){
 
 void init (GLFWwindow* window){
     renderingProgram = createShaderProgram((char *)"vertShader.glsl",(char *) "fragShader.glsl");
-    cameraX = 0.0f; cameraY = 1.0f; cameraZ = 0.0f;
+    cameraX = 0.0f; cameraY = 4.0f; cameraZ = 0.0f;
     lookingDirX = 0; lookingDirY = 0; lookingDirZ = -1;
     setupVertices();
 }
@@ -108,36 +111,35 @@ void display (GLFWwindow* window, double currentTime){
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
-    cout<<"KEY:"<<key<<" - "<<glfwGetKeyScancode(key)<<endl;
+    //cout<<"KEY:"<<key<<" - "<<glfwGetKeyScancode(key)<<endl;
     /*FrecciaSu : 265
      *FrecciaGiu : 264
      *FrecciaSx : 263
      *FrecciaDx : 262
     */
-    if (glfwGetKey(window, 265) == GLFW_PRESS){
-        //cameraZ-=PASSOCAMERA;
-        //cameraZ+=(lookingZ-cameraZ)/(lookingX*lookingX+lookingY*lookingY+lookingZ*lookingZ);
-        //cameraX+=(lookingX-cameraX)/(lookingX*lookingX+lookingY*lookingY+lookingZ*lookingZ);
-        //cameraY+=(lookingY-cameraY)/(lookingX*lookingX+lookingY*lookingY+lookingZ*lookingZ);
+    if (glfwGetKey(window, 265) == GLFW_PRESS){//freccia su
     }
-    if (glfwGetKey(window, 264) == GLFW_PRESS){
-        //cameraZ+=PASSOCAMERA;
+    if (glfwGetKey(window, 264) == GLFW_PRESS){//freccia giu
     }
-    if (glfwGetKey(window, 263) == GLFW_PRESS){
-        cameraX=cameraX-PASSOCAMERA;
+    if (glfwGetKey(window, 263) == GLFW_PRESS){//freccia sinistra
     }
-    if (glfwGetKey(window, 262) == GLFW_PRESS){
-        cameraX=cameraX+PASSOCAMERA;
+    if (glfwGetKey(window, 262) == GLFW_PRESS){//freccia destra
     }
-    if (glfwGetKey(window, 334) == GLFW_PRESS ||
-        glfwGetKey(window, 93) == GLFW_PRESS){
-        scale+=PASSOSCALE;
+    if (glfwGetKey(window, 87) == GLFW_PRESS){//W
+        cameraX += lookingDirX*PASSOCAMERA;
+        //cameraY += lookingDirY*PASSOCAMERA;
+        cameraZ += lookingDirZ*PASSOCAMERA;
     }
-    if (glfwGetKey(window, 333) == GLFW_PRESS ||
-        glfwGetKey(window, 47) == GLFW_PRESS){
-        if (scale-PASSOSCALE!=0){
-          scale-=PASSOSCALE;
-        }
+    if (glfwGetKey(window, 83) == GLFW_PRESS){//S
+        cameraX -= lookingDirX*PASSOCAMERA;
+        //cameraY += lookingDirY*PASSOCAMERA;
+        cameraZ -= lookingDirZ*PASSOCAMERA;
+    }
+    if (glfwGetKey(window, 65) == GLFW_PRESS){//A
+
+    }
+    if (glfwGetKey(window, 68) == GLFW_PRESS){//D
+
     }
     if (glfwGetKey(window, 256) == GLFW_PRESS){
         if (mouseblocked){
@@ -153,38 +155,48 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos){
   //NON DEVO RUOTARE ATTORNO AD X MA ATTORNO AD UN ASSE CHE VARIA
   if (mouseblocked){
+    glm::vec3 newlookingDir(lookingDirX, lookingDirY, lookingDirZ);
     float newlookingDirX = lookingDirX;
     float newlookingDirZ = lookingDirZ;
     float newlookingDirY = lookingDirY;
     float thetax = PASSOVISUALE*abs(backmousex-xpos)/50;
     float thetay = PASSOVISUALE*abs(backmousey-ypos)/200;
     if (xpos>backmousex){
-      newlookingDirX = cos(thetax)*lookingDirX+
+      newlookingDir = rotate(0, 1, 0, glm::vec3(lookingDirX, lookingDirY, lookingDirZ), thetax);
+      //cout << newlookingDirX;
+      /*newlookingDirX = cos(thetax)*lookingDirX+
       sin(thetax)*lookingDirZ;
+      cout << "; " << newlookingDirX << endl;
       newlookingDirZ = -sin(thetax)*lookingDirX+
-      cos(thetax)*lookingDirZ;
+      cos(thetax)*lookingDirZ;*/
     }
     if (xpos<backmousex){
-      newlookingDirX = cos(-thetax)*lookingDirX+
+      newlookingDir = rotate(0, 1, 0, glm::vec3(lookingDirX, lookingDirY, lookingDirZ), -thetax);
+      /*newlookingDirX = cos(-thetax)*lookingDirX+
       sin(-thetax)*lookingDirZ;
       newlookingDirZ = -sin(-thetax)*lookingDirX+
-      cos(-thetax)*lookingDirZ;
+      cos(-thetax)*lookingDirZ;*/
     }
     if (ypos>backmousey && atan(lookingDirY/(sqrt(lookingDirX*lookingDirX+lookingDirZ*lookingDirZ)))>-0.785398f){
-      newlookingDirY = cos(thetay)*lookingDirY-
+      newlookingDir = rotate(-(lookingDirZ)/sqrt(lookingDirX*lookingDirX+lookingDirZ*lookingDirZ), 0, (lookingDirX)/sqrt(lookingDirX*lookingDirX+lookingDirZ*lookingDirZ), glm::vec3(lookingDirX, lookingDirY, lookingDirZ), thetay);
+      /*newlookingDirY = cos(thetay)*lookingDirY-
       sin(thetay)*lookingDirZ;
       newlookingDirZ = sin(thetay)*lookingDirY+
-      cos(thetay)*lookingDirZ;
+      cos(thetay)*lookingDirZ;*/
     }
-    if (ypos<backmousey){
-      newlookingDirY = cos(-thetay)*lookingDirY-
+    if (ypos<backmousey && atan(lookingDirY/(sqrt(lookingDirX*lookingDirX+lookingDirZ*lookingDirZ)))<0.785398f){
+      newlookingDir = rotate(-(lookingDirZ)/sqrt(lookingDirX*lookingDirX+lookingDirZ*lookingDirZ), 0, (lookingDirX)/sqrt(lookingDirX*lookingDirX+lookingDirZ*lookingDirZ), glm::vec3(lookingDirX, lookingDirY, lookingDirZ), -thetay);
+      /*newlookingDirY = cos(-thetay)*lookingDirY-
       sin(-thetay)*lookingDirZ;
       newlookingDirZ = sin(-thetay)*lookingDirY+
-      cos(-thetay)*lookingDirZ;
+      cos(-thetay)*lookingDirZ;*/
     }
-    lookingDirX = newlookingDirX/sqrt(newlookingDirX*newlookingDirX+newlookingDirZ*newlookingDirZ+newlookingDirY*newlookingDirY);
+    /*lookingDirX = newlookingDirX/sqrt(newlookingDirX*newlookingDirX+newlookingDirZ*newlookingDirZ+newlookingDirY*newlookingDirY);
     lookingDirZ = newlookingDirZ/sqrt(newlookingDirX*newlookingDirX+newlookingDirZ*newlookingDirZ+newlookingDirY*newlookingDirY);
-    lookingDirY = newlookingDirY/sqrt(newlookingDirX*newlookingDirX+newlookingDirZ*newlookingDirZ+newlookingDirY*newlookingDirY);
+    lookingDirY = newlookingDirY/sqrt(newlookingDirX*newlookingDirX+newlookingDirZ*newlookingDirZ+newlookingDirY*newlookingDirY);*/
+    lookingDirX = newlookingDir.x;
+    lookingDirY = newlookingDir.y;
+    lookingDirZ = newlookingDir.z;
     backmousex = xpos;
     backmousey = ypos;
     //cout << xpos << ";" << ypos << endl;
@@ -222,4 +234,11 @@ int main(void){
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
+}
+
+glm::vec3 rotate(float x, float y, float z, glm::vec3 vec, float T){
+  return glm::mat3( x*x+(1-x*x)*cos(T),       x*y*(1-cos(T))-z*sin(T),   x*z*(1-cos(T))+y*sin(T),
+                    x*y*(1-cos(T))+z*sin(T),  y*y+(1-y*y)*cos(T),       y*z*(1-cos(T))-x*sin(T),
+                    x*z*(1-cos(T))-y*sin(T),  y*z*(1-cos(T))+x*sin(T),  z*z+(1-z*z)*cos(T)
+                  )*vec;
 }
