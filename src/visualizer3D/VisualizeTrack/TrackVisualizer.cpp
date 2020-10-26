02c930340ad78049fbb092de5a226f45a314f22a
Nut <enrico.mannocci99@gmail.com>
2020-10-26 09:00:41 +0100

    Telecamera si muove con Mouse (Y ancora sbagliato)

--- src/visualizer3D/VisualizeTrack/TrackVisualizer.cpp
+++ src/visualizer3D/VisualizeTrack/TrackVisualizer.cpp
@@ -16,11 +16,17 @@ using namespace std;
 #define numVBOs 2
 #define PASSOSCALE 0.1f
 #define PASSOCAMERA 1.0f
+#define PASSOVISUALE 0.1f
 //VAO = Vertex Array Objects
 //VBO = Vertex Buffer Objects
 
 float scale = 1.0f;
 float cameraX , cameraY, cameraZ;
+float lookingDirX, lookingDirY, lookingDirZ;
+
+float backmousex=0, backmousey=0;
+bool mouseblocked = false;
+
 float * vertices;
 int NumOfVertices;
 GLuint renderingProgram; //GLuint è una shortcat per unsigned int
@@ -59,11 +65,14 @@ void setupVertices(){
 
 void init (GLFWwindow* window){
     renderingProgram = createShaderProgram((char *)"vertShader.glsl",(char *) "fragShader.glsl");
-    cameraX = 0.0f; cameraY = 2.0f; cameraZ = 10.0f;
+    cameraX = 0.0f; cameraY = 1.0f; cameraZ = 0.0f;
+    lookingDirX = 0; lookingDirY = 0; lookingDirZ = -1;
     setupVertices();
 }
 
 void display (GLFWwindow* window, double currentTime){
+
+    //lookingDirX = 10*cos((float)currentTime);
     glClear(GL_DEPTH_BUFFER_BIT);
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT); //Clear the background to black
@@ -79,7 +88,7 @@ void display (GLFWwindow* window, double currentTime){
     pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); //1.0472 radians = 60 degrees
 
     //Costruisco la mvMat
-    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+    vMat = glm::lookAt(glm::vec3(cameraX, cameraY, cameraZ), glm::vec3(cameraX+lookingDirX, cameraY+lookingDirY, cameraZ+lookingDirZ), glm::vec3(0.0f, 1.0f, 0.0f));
     mMat = glm::rotate(glm::mat4(1.0f), 0.785398f*2, glm::vec3(-1.0f, 0.0f, 0.0f));
     mMat = glm::translate(mMat, glm::vec3(-vertices[0]*scale, -vertices[1]*scale, 0));
     mMat = glm::scale(mMat, glm::vec3( scale, scale, scale));
@@ -99,17 +108,20 @@ void display (GLFWwindow* window, double currentTime){
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
-    //cout<<"KEY:"<<key<<" - "<<glfwGetKeyScancode(key)<<endl;
+    cout<<"KEY:"<<key<<" - "<<glfwGetKeyScancode(key)<<endl;
     /*FrecciaSu : 265
      *FrecciaGiu : 264
      *FrecciaSx : 263
      *FrecciaDx : 262
     */
     if (glfwGetKey(window, 265) == GLFW_PRESS){
-        cameraZ-=PASSOCAMERA;
+        //cameraZ-=PASSOCAMERA;
+        //cameraZ+=(lookingZ-cameraZ)/(lookingX*lookingX+lookingY*lookingY+lookingZ*lookingZ);
+        //cameraX+=(lookingX-cameraX)/(lookingX*lookingX+lookingY*lookingY+lookingZ*lookingZ);
+        //cameraY+=(lookingY-cameraY)/(lookingX*lookingX+lookingY*lookingY+lookingZ*lookingZ);
     }
     if (glfwGetKey(window, 264) == GLFW_PRESS){
-        cameraZ+=PASSOCAMERA;
+        //cameraZ+=PASSOCAMERA;
     }
     if (glfwGetKey(window, 263) == GLFW_PRESS){
         cameraX=cameraX-PASSOCAMERA;
@@ -127,8 +139,62 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
           scale-=PASSOSCALE;
         }
     }
+    if (glfwGetKey(window, 256) == GLFW_PRESS){
+        if (mouseblocked){
+          glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+          mouseblocked = false;
+        }else{
+          glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+          mouseblocked = true;
+        }
+    }
 }
 
+static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos){
+  //NON DEVO RUOTARE ATTORNO AD X MA ATTORNO AD UN ASSE CHE VARIA
+  if (mouseblocked){
+    float newlookingDirX = lookingDirX;
+    float newlookingDirZ = lookingDirZ;
+    float newlookingDirY = lookingDirY;
+    float thetax = PASSOVISUALE*abs(backmousex-xpos)/50;
+    float thetay = PASSOVISUALE*abs(backmousey-ypos)/200;
+    if (xpos>backmousex){
+      newlookingDirX = cos(thetax)*lookingDirX+
+      sin(thetax)*lookingDirZ;
+      newlookingDirZ = -sin(thetax)*lookingDirX+
+      cos(thetax)*lookingDirZ;
+    }
+    if (xpos<backmousex){
+      newlookingDirX = cos(-thetax)*lookingDirX+
+      sin(-thetax)*lookingDirZ;
+      newlookingDirZ = -sin(-thetax)*lookingDirX+
+      cos(-thetax)*lookingDirZ;
+    }
+    if (ypos>backmousey && atan(lookingDirY/(sqrt(lookingDirX*lookingDirX+lookingDirZ*lookingDirZ)))>-0.785398f){
+      newlookingDirY = cos(thetay)*lookingDirY-
+      sin(thetay)*lookingDirZ;
+      newlookingDirZ = sin(thetay)*lookingDirY+
+      cos(thetay)*lookingDirZ;
+    }
+    if (ypos<backmousey){
+      newlookingDirY = cos(-thetay)*lookingDirY-
+      sin(-thetay)*lookingDirZ;
+      newlookingDirZ = sin(-thetay)*lookingDirY+
+      cos(-thetay)*lookingDirZ;
+    }
+    lookingDirX = newlookingDirX/sqrt(newlookingDirX*newlookingDirX+newlookingDirZ*newlookingDirZ+newlookingDirY*newlookingDirY);
+    lookingDirZ = newlookingDirZ/sqrt(newlookingDirX*newlookingDirX+newlookingDirZ*newlookingDirZ+newlookingDirY*newlookingDirY);
+    lookingDirY = newlookingDirY/sqrt(newlookingDirX*newlookingDirX+newlookingDirZ*newlookingDirZ+newlookingDirY*newlookingDirY);
+    backmousex = xpos;
+    backmousey = ypos;
+    //cout << xpos << ";" << ypos << endl;
+  }
+}
+void window_size_callback(GLFWwindow* window, int newWidth, int newHeight){
+  aspect = (float)newWidth/(float)newHeight;
+  glViewport(0, 0, newWidth, newHeight);
+  pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); //1.0472 radians = 60 degrees
+}
 int main(void){
     if (!glfwInit()) {exit(EXIT_FAILURE);}
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -143,6 +209,10 @@ int main(void){
     NumOfVertices = TD.getNumOfVertices();
     init(window);
     glfwSetKeyCallback(window, key_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    mouseblocked=true;
+    glfwSetWindowSizeCallback(window, window_size_callback);
 
     while (!glfwWindowShouldClose(window)) {
         display(window, glfwGetTime());
